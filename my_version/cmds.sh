69b851673ad10d1fb3fdf7736013e1bd97e362cb
Alex <alexliu0809@hotmail.com>
2022-03-29 11:49:43 -0700

    Firebase

--- my_version/cmds.sh
+++ my_version/cmds.sh
@@ -2,3 +2,4 @@ npx create-react-app ./
 yarn start
 npm install @material-ui/core
 yarn add @material-ui/core @material-ui/core/icons
+yarn add firebase
--- my_version/my_gmail_clone/src/components/signin/signin.js
+++ my_version/my_gmail_clone/src/components/signin/signin.js
@@ -1,6 +1,7 @@
 import { Button, TextField } from "@material-ui/core"
 import React, { useState } from 'react'
 import Signup from '../signup/signup.js'
+import { auth } from "../../lib/firebase"
 
 import './styles.css'
 
@@ -11,7 +12,11 @@ const Signin = () =>{
     // setShowSignup is of class SetStateAction
     const [showSignup, setShowSignup] = useState(false);
     const [loadingTime, setloadingTime] = useState(false);
-
+    const [userEmail, setuserEmail] = useState('');
+    const [userPassword, setuserPassword] = useState('');
+    const [emailFormatError, setemailFormatError] = useState('')
+    const [passwordWrongError, setpasswordWrongError] = useState({state:false,msg:''})
+    
     // whenever user click on create account, this function is triggered
     const toggleSignup = (e)=>{
         // prevent page from freshing
@@ -21,6 +26,28 @@ const Signin = () =>{
         //setShowSignup(true)
     };
 
+    const firebaseSignin =(e) => {
+      e.preventDefault();
+      setloadingTime(true);
+      console.log("Firebase signin")
+      console.log(showSignup, userEmail, userPassword)
+
+      auth.signInWithEmailAndPassword(userEmail, userPassword)
+      .then(() => {
+        alert('logged in')
+      })
+      .catch((err) => {
+        setloadingTime(false);
+        console.log(err.message)
+        console.log(err.code)
+        if (err.code === "auto/wrong-password"){
+          setpasswordWrongError({state:true, msg:"Wrong Password"})
+        }
+        if (err.code === "auth/invalid-value-(email),-starting-an-object-on-a-scalar-field"){
+          setemailFormatError({state:true, msg:"Invalid Email"})
+        }
+      });
+    }
     
 
     // If else statement based on state: {setShowSignup ? () : () }
@@ -49,7 +76,10 @@ const Signin = () =>{
                   variant="outlined"
                   type="email"
                   className="login__input"
-          
+                  value={userEmail}
+                  onChange={(e) => setuserEmail(e.target.value)}
+                  error={emailFormatError.state}
+                  helperText={emailFormatError.msg}
                 />
   
                 <TextField
@@ -58,6 +88,10 @@ const Signin = () =>{
                   variant="outlined"
                   className="login__input"
                   type="password"
+                  value={userPassword}
+                  onChange={(e) => setuserPassword(e.target.value)}
+                  error={passwordWrongError.state}
+                  helperText={passwordWrongError.msg}
                 />
   
                 <div className="login__infoText">
@@ -78,6 +112,7 @@ const Signin = () =>{
                     className="login__button"
                     color="primary"
                     variant="contained"
+                    onClick={firebaseSignin}
                   >
                     Sign in
                   </Button>
